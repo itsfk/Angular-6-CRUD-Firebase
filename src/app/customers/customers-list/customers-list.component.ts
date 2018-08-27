527fdfe9f2680d3296bdc02444eb9916697fb4b7
Faiz <faiz.ggmu@gmail.com>
2018-08-27 17:55:45 +0500

    Customer list Component Updated

--- src/app/customers/customers-list/customers-list.component.ts
+++ src/app/customers/customers-list/customers-list.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
-
+import { map } from 'rxjs/operators';
+import { CustomerService } from '../customer.service';
 @Component({
   selector: 'app-customers-list',
   templateUrl: './customers-list.component.html',
   styleUrls: ['./customers-list.component.css']
 })
 export class CustomersListComponent implements OnInit {
-
-  constructor() { }
-
+  customers: any;
+   constructor(private customerService: CustomerService) { }
   ngOnInit() {
+  this.getCustomersList();
   }
-
+  getCustomersList() {
+    // Use snapshotChanges().map() to store the key
+    this.customerService.getCustomersList().snapshotChanges().pipe(
+      map(changes =>
+        changes.map(c => ({ key: c.payload.key, ...c.payload.val() }))
+      )
+    ).subscribe(customers => {
+      this.customers = customers;
+    });
+  }
+  deleteCustomers() {
+    this.customerService.deleteAll();
+  }
 }
