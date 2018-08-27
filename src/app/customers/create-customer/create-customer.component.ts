2e02c7cf2a0def251307e9eab2cfc87c7bdeef2f
Faiz <faiz.ggmu@gmail.com>
2018-08-27 18:01:36 +0500

    Create Customer  updated

--- src/app/customers/create-customer/create-customer.component.ts
+++ src/app/customers/create-customer/create-customer.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Customer } from '../customer';
+import { CustomerService } from '../customer.service';
 
 @Component({
   selector: 'app-create-customer',
@@ -6,10 +9,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./create-customer.component.css']
 })
 export class CreateCustomerComponent implements OnInit {
-
-  constructor() { }
+  customer: Customer = new Customer();
+  submitted = false;
+  constructor(private customerService: CustomerService) { }
 
   ngOnInit() {
   }
-
+  newCustomer(): void {
+    this.submitted = false;
+    this.customer = new Customer();
+  }
+  save() {
+    this.customerService.createCustomer(this.customer);
+    this.customer = new Customer();
+  }
+  onSubmit() {
+    this.submitted = true;
+    this.save();
+  }
 }
